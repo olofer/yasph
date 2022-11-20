b298cbbc1d3712ea806cd6735bbe65c27dd8a85e
olofer <k.e.j.olofsson@gmail.com>
2022-11-20 14:50:56 +0100

    adjust demo script

--- scripts/demo-gravity.sh
+++ scripts/demo-gravity.sh
@@ -3,8 +3,8 @@
 # Low-pressure Helium and Xenon
 # usage: ./demo-gravity.sh numpergas
 
-python3 ../scripts/yasph-init-ball.py --num-per-gas $1 --particle-file gdemo-parts.yasph --parameter-file gdemo-params.yasph --barrier-radius 1.00 --density 0.1786 5.851 --pressure 1e2 1e2
+python3 ../scripts/yasph-init-ball.py --num-per-gas $1 --particle-file gdemo-parts.yasph --parameter-file gdemo-params.yasph --barrier-radius 1.00 --density 0.1786 5.851 --pressure 10 10
 ./yasph gdemo-parts.yasph gdemo-params.yasph steps=0 param-file=gdemo-params-explicit.yasph frame-steps=50 frame-file=gravity.yasph viscosity=1 epsbn=0.0 epsbt=0.0 trace-steps=50 trace-file=gravity-trace.yasph verbosity=0
 
 # Pass in a comprehensive set of parameters (and additionally override a few options on command line; typical usage)
-./yasph gdemo-parts.yasph gdemo-params-explicit.yasph steps=5e4 dt=4.0e-6 threads=4 verbosity=1 gravity=0.0,-9.82 > gravity-log.yasph 
+./yasph gdemo-parts.yasph gdemo-params-explicit.yasph steps=5e4 dt=1.0e-5 threads=4 verbosity=1 gravity=0.0,-9.82 > gravity-log.yasph 
